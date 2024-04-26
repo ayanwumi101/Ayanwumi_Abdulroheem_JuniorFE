c04309ab70c1cf6c85f02f2275bd406ff6d0106f
abdulroheem <ayanwunmiabdulroheem@gmail.com>
2024-04-26 11:19:07 +0100

    fix: made the search bar responsive

--- src/lib/components/SearchBar/index.tsx
+++ src/lib/components/SearchBar/index.tsx
@@ -6,10 +6,10 @@ import { SearchBarProps } from '~/lib/utilities/schema'
 const SearchBar = ({searchQuery, setSearchQuery}: SearchBarProps) => {
 
   return (
-    <Box w='600px' mx='auto' border='2px solid #ededed' borderRadius='60px' py='2' px='3'>
+    <Box maxW='600px' mx='auto' border='2px solid #ededed' borderRadius='60px' py='2' px='3'>
         <Flex alignItems='center'>
             <RiSearch2Line fontSize={30} color='gray' />
-            <input value={searchQuery} onChange={(e: any) => setSearchQuery(e.target.value)} type="text" style={{width: '450px', border: 'none', outline: 'none', padding: '10px', backgroundColor: 'none'}} placeholder='Search capsules by status, type and serial no' />
+            <input value={searchQuery} onChange={(e: any) => setSearchQuery(e.target.value)} type="text" style={{width: '500px', border: 'none', outline: 'none', padding: '10px', backgroundColor: 'none'}} placeholder='Search capsules by status, type and serial no' />
         </Flex>
     </Box>
   )
